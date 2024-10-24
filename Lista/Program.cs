fe5e4b7a54c8f8a659dbce78fe9e793038205a06
CaioK367 <130302373+CaioK367@users.noreply.github.com>
2024-10-24 15:55:05 -0300

    Add project files.

---
+++ Lista/Program.cs
@@ -0,0 +1,48 @@
+﻿using Lista;
+
+Tarefa t = new Tarefa();
+
+static void Menu()
+{
+    Console.WriteLine("Olá! Seja bem vindo a lista de tarefas");
+    Console.WriteLine("O que deseja fazer?");
+    Console.WriteLine("1 - Adicionar tarefa");
+    Console.WriteLine("2 - Remover tarefa");
+    Console.WriteLine("3 - Listar tarefas");
+    Console.WriteLine("4 - Sair");
+}
+
+bool continuar = true;
+while (continuar) {
+    Menu();
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            t.AdicionarTarefa();
+            break;
+        case "2":
+            t.RemoverTarefa();
+            break;
+        case "3":
+            t.Exibir();
+            break;
+        case "4":
+            continuar = false;
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Lista/Tarefa.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    internal class Tarefa
+    {
+        public string Nome { get; set; }
+
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+        public Tarefa() { }
+
+
+        public Tarefa(string nome, string descricao, bool concluida)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Concluida = concluida;
+        }
+
+        public Tarefa(string nome, bool concluida)
+        {
+            Nome = nome;
+            Concluida = concluida;
+        }
+
+        List<Tarefa> tarefas = new List<Tarefa>();
+
+        public void AdicionarTarefa()
+        {
+            Console.Clear();
+            Console.Write("Qual a tarefa? : ");
+            string nome = Console.ReadLine();
+
+            while (nome == "")
+            {
+
+                if (nome == "")
+                {
+                    Console.WriteLine("O nome da tarefa não pode ser vazio!!");
+                    Console.Write("Qual a tarefa? : ");
+                    nome = Console.ReadLine();
+                }
+            }
+
+            Console.Write("Descrição da tarefa: ");
+            string descricao = Console.ReadLine();
+
+            try 
+            { 
+                tarefas.Add(new Tarefa(nome, descricao, false));
+                Console.WriteLine("Tarefa Adicionada com Sucesso!");
+                Console.Write("");
+                Thread.Sleep(2000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+            Console.Clear();
+
+        }
+
+        public void RemoverTarefa()
+        {
+            
+
+            if (tarefas.Count == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Nenhuma tarefa adicionada");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+            else
+            {
+                foreach (var tarefa in tarefas)
+                {
+                    Exibir();
+                }
+                Console.WriteLine("Qual tarefa deseja remover?");
+                int index = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    tarefas.RemoveAt(index);
+                    Console.WriteLine("Tarefa removida com sucesso!");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        public void Exibir()
+        {
+            Console.Clear();
+
+            if (tarefas.Count == 0)
+            {
+                Console.WriteLine("Não foi adicionada nenhuma tarefa!");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                for (int i = 0; i < tarefas.Count; i++)
+                {
+                    var tarefa = tarefas[i];
+                    Console.WriteLine($"{i} - {tarefa.Nome} - {tarefa.Descricao} - {tarefa.Concluida}");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+        }
+
+        
+    }
+}
